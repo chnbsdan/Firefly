4f76bfc643d5a3fa390835ff3d45fa151afece7a
chnbsdan <chnbsdan@gmail.com>
2025-11-08 21:36:05 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -90,7 +90,7 @@ export const siteConfig: SiteConfig = {
     // 背景图片配置
     src: {
       // 桌面背景图片
-      desktop: "/assets/images/d1.webp",
+      desktop: "/assets/images/yk7.jpg",
       // 移动背景图片
       mobile: "/assets/images/m1.webp",
     },
