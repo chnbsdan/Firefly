84a2d6070e370e7a8a76949c00af0657b8de544c
chnbsdan <chnbsdan@gmail.com>
2025-11-08 18:18:36 +0800

    Update sponsorConfig.ts

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -17,7 +17,7 @@ export const sponsorConfig: SponsorConfig = {
       name: "支付宝",
       icon: "fa6-brands:alipay",
       qrCode: "/assets/images/sponsor/alipay.png", // 收款码图片路径（需要放在 public 目录下）
-      link: "",
+      link: "https://tc.1356666.xyz/file/1762597019001_支付宝new.png",
       description: "使用 支付宝 扫码赞助",
       enabled: true,
     },
@@ -25,7 +25,7 @@ export const sponsorConfig: SponsorConfig = {
       name: "微信",
       icon: "fa6-brands:weixin",
       qrCode: "/assets/images/sponsor/wechat.png", // 收款码图片路径
-      link: "",
+      link: "https://tc.1356666.xyz/file/1762597014272_微信new.png",
       description: "使用 微信 扫码赞助",
       enabled: true,
     },
@@ -33,7 +33,7 @@ export const sponsorConfig: SponsorConfig = {
       name: "爱发电",
       icon: "simple-icons:afdian",
       qrCode: "",
-      link: "https://afdian.com/a/cuteleaf",
+      link: "https://afdian.com/a/hangdn",
       description: "通过 爱发电 进行赞助",
       enabled: true,
     },
@@ -41,7 +41,7 @@ export const sponsorConfig: SponsorConfig = {
       name: "Github",
       icon: "fa6-brands:github",
       qrCode: "",
-      link: "https://github.com/CuteLeaf/Firefly",
+      link: "https://github.com/chnbsdan/Firefly",
       description: "点个Star就是最大的支持",
       enabled: true,
     },
@@ -51,9 +51,9 @@ export const sponsorConfig: SponsorConfig = {
   sponsors: [
     // 示例：已实名赞助者
     {
-      name: "夏叶",
+      name: "bsdan",
       amount: "¥50",
-      date: "2025-10-01",
+      date: "2025-08-01",
       message: "感谢分享！",
     },
     // 示例：匿名赞助者
